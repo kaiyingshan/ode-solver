e6eb1b7c4c6e6e55fc285e53beeb362737993d18
kaiyingshan <shan_kaiying@163.com>
2019-02-17 18:02:31 -0500

    bootstrapped ode api endpoint front end scaffolding

--- root/views.py
+++ root/views.py
@@ -18,29 +18,39 @@ def index(request):
 
 def procedure_to_dict(procedure: solver.Procedure):
     return {
-        procedure[i][0]: latex(procedure[i][1]) for i in range(len(procedure))
+        procedure[i][0]: [latex(a) for a in procedure[i][1]] for i in range(len(procedure))
     }
 
 
 def ode(request: HttpRequest):
     if request.method == "POST":
-        r: str = request.POST["r"]
+
         p: str = request.POST["p"]
         q: str = request.POST["q"]
+        r: str = request.POST["r"]
         g: str = request.POST["g"]
 
-        if r == "0":
+        if p == "0":
             # first order
-            if "y" not in q and "y" not in q and "y" not in g:
+            if "y" not in q and "y" not in r and "y" not in g:
                 # linear
                 pass
             pass
         else:
             try:
                 # second order const coeff
-                a = float(r)
-                b = float(p)
-                c = float(q)
+                a = float(p)
+                b = float(q)
+                c = float(r)
+
+                if a.is_integer():
+                    a = int(a)
+
+                if b.is_integer():
+                    b = int(b)
+
+                if c.is_integer():
+                    c = int(c)
 
                 y1, y2, procedure = solver.sec_order_const_coeff(a, b, c)
 
